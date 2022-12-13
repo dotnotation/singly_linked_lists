0ac8ca47b2919d943aa6a413999191c5145e33ad
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-13 15:35:15 -0500

    started coding Node class

---
+++ singly_linked_list.js
@@ -0,0 +1,8 @@
+class Node{
+    constructor(val){
+        // a node is just a value
+        this.val = val
+        // it also has a reference to the next node
+        this.next = null
+    }
+}
